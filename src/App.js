2ec7a77814de18655d6785f4f21fa11579463ab6
Yin <yinnnn17@gmail.com>
2021-06-12 23:25:26 -0700

    Added grid square

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import GridSquare from './components/GridSquare'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
+      <GridSquare color="1" />
     </div>
   );
 }
