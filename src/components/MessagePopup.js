bc381716dfdb974e6e00ad75a0fea4678e2d921a
Yin <yinnnn17@gmail.com>
2021-06-13 12:59:27 -0700

    Added initial message popup

---
+++ src/components/MessagePopup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Displays a message
+export default function MessagePopup(props) {
+  return (
+    <div className='message-popup'>
+      <h1>Message Title</h1>
+      <p>Message info...</p>
+    </div>
+  )
+}
