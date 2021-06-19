e5f9c7df14c6c6763d61f38ebcd93c065affb621
Yin <yinnnn17@gmail.com>
2021-06-18 23:20:38 -0700

    deploy the app

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -83,8 +83,8 @@ const gameReducer = (state = defaultState(), action) => {
       return state
 
     case RESTART:
-
-      return state
+      
+      return defaultState()
 
     default:
       return state
